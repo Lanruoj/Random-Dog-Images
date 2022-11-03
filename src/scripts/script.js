706efa1455164ea57b2f424c05b6ba7559d87ee0
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-11-03 16:55:09 +1100

    Implement basic solution (doesn't refresh page)
    - On button click, parses text input and fetches interpolated URL
    - Displays three images
    - [to fix]  Recursively adds images without refreshing page

---
+++ src/scripts/script.js
@@ -0,0 +1,23 @@
+function displayImgs(data) {
+    let images = data.message
+    images.forEach((img, index) => {
+        let newImg = document.createElement("img")
+        newImg.id = `dog-img${index+1}`
+        newImg.src = img
+        document.body.appendChild(newImg)
+    })
+}
+
+
+const button = document.querySelector("#generate")
+
+button.addEventListener("click", (event) => {
+    event.preventDefault()
+    let breedInput = document.querySelector("#breed-input").value
+    fetch(`https://dog.ceo/api/breed/${breedInput}/images/random/3`)
+        .then(body => {
+            return body.json()
+        })
+        .then(data => displayImgs(data))
+        .catch(error => console.log(error))
+})
